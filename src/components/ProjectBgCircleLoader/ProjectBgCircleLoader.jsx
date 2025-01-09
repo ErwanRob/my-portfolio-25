cadca8c1db0381e70928590ba99226e4e81eec03
Erwan ROBIN <erobin.pro@outlook.fr>
2025-01-09 22:44:10 +0100

    Vercel debug test

--- src/components/ProjectBgCircleLoader/ProjectBgCircleLoader.jsx
+++ src/components/ProjectBgCircleLoader/ProjectBgCircleLoader.jsx
@@ -1,7 +1,9 @@
 import styles from "./ProjectBgCircleLoader.module.scss";
 
 const ProjectCircleLoad = () => {
-  return <div className={styles.projectBgCircleLoader} />;
+  return (
+    <div className={styles.projectBgCircleLoader}>hi, testing for vercel</div>
+  );
 };
 
 export default ProjectCircleLoad;
