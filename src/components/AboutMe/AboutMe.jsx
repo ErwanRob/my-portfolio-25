927ffc98b8327785d322ca435d764051f9b39a1e
Erwan ROBIN <erobin.pro@outlook.fr>
2025-01-14 10:14:57 +0100

    Add react-intersection-observer package; introduce MaskText component styles and background assets for enhanced visual effects. Worked on AboutMe section.

--- src/components/AboutMe/AboutMe.jsx
+++ src/components/AboutMe/AboutMe.jsx
@@ -1,16 +1,144 @@
 import styles from "./AboutMe.module.scss";
 import portrait from "../../assets/img/profilp/img5.jpg";
+import MaskText from "../MaskText/MaskText";
+import { motion } from "framer-motion";
+
+const phrasesHeadline = ["Hi, I’m a passionate front-end developer,"];
+const hrasesHeadlineBis = [
+  "dedicated to crafting engaging and user-friendly websites.",
+];
+const phraseBuildSomething = ["Let’s create something together."];
+
+/* const phrases2 = [
+  "I specialize in blending clean, modern design with seamless functionality",
+  " to deliver solutions that are not only visually appealing but also highly effective.",
+];
+const phrases3 = [
+  "Whether it’s building responsive layouts, creating intuitive user interfaces,",
+  "optimizing websites for performance and SEO, my focus on delivering",
+  "measurable value to my clients. I take pride in solving problems and",
+  "turning ideas into reality with precision and creativity.",
+];
+const phrases4 = [
+  "I’m committed to staying at the forefront of web development",
+  "practices and helping clients achieve their vision with impactful,",
+  "efficient, and scalable web solutions.",
+]; */
+/* const spreadPhrase5 = phrases5.flatMap((phrase) => phrase.split(" ")); */
+
+const phrases1 = [
+  "I specialize in blending clean, modern design with seamless functionality",
+];
+const phrases2 = [
+  "to deliver solutions that are not only visually appealing but also highly effective.",
+];
+
+const phrases3 = [
+  "Whether it’s building responsive layouts, creating intuitive user interfaces,",
+];
+const phrases4 = [
+  "or optimizing websites for performance and SEO, my focus is on delivering",
+];
+
+const phrases5 = [
+  "measurable value to my clients. I take pride in solving problems and turning",
+];
+const phrases6 = ["ideas into reality with precision and creativity."];
+
+const phrases7 = [
+  "I’m committed to staying at the forefront of web development practices,",
+];
+const phrases8 = [
+  "helping clients achieve their vision with impactful, efficient, scalable solutions.",
+];
 
 const AboutMe = () => {
   return (
     <div className={`section ${styles.aboutMe}`} id="about-me">
-      <div className={styles["aboutMe__txtContent"]}>
-        <h3 className={styles["aboutMe__txtContent__title"]}>About Me</h3>
-        <div className={styles["aboutMe__txtContent__paraWrapper"]}>
-          <p className={styles["aboutMe__txtContent__paraWrapper__paraOne"]}>
+      <div className={styles["aboutMe__container"]}>
+        <div className={styles["aboutMe__container__headLine"]}>
+          <MaskText phrases={phrasesHeadline} variant="primary" align="left" />
+          <MaskText
+            phrases={hrasesHeadlineBis}
+            variant="primary"
+            align="right"
+          />
+        </div>
+        <motion.div
+          className={styles["aboutMe__container__midWrapper"]}
+          initial={{ x: "-2rem" }}
+          whileInView={{ x: 0 }}
+          transition={{ duration: 0.6 }}
+        >
+          <motion.h3
+            className={styles["aboutMe__container__midWrapper__cardTitle"]}
+            initial={{ y: "-1rem" }}
+            whileInView={{ y: 0 }}
+            transition={{ duration: 0.6, delay: 0.1 }}
+          >
+            ABOUT ME
+          </motion.h3>
+          <div className={styles["aboutMe__container__midWrapper__profil"]}>
+            <div
+              className={
+                styles["aboutMe__container__midWrapper__profil__imgContainer"]
+              }
+            >
+              <img
+                className={
+                  styles[
+                    "aboutMe__container__midWrapper__profil__imgContainer__img"
+                  ]
+                }
+                src={portrait}
+                alt="selfPortrait"
+              />
+            </div>
+          </div>
+          <div className={styles["aboutMe__container__midWrapper__info"]}>
+            <div
+              className={
+                styles["aboutMe__container__midWrapper__info__titleTogether"]
+              }
+            >
+              <MaskText
+                phrases={phraseBuildSomething}
+                variant="tertiary"
+                align="right"
+              />
+            </div>
+            <div
+              className={
+                styles["aboutMe__container__midWrapper__info__masksWrapper"]
+              }
+            >
+              {/*  <MaskText phrases={phrases2} variant="secondary" align="left" />
+              <MaskText phrases={phrases3} variant="secondary" align="left" />
+              <MaskText phrases={phrases4} variant="secondary" align="left" /> */}
+              <MaskText phrases={phrases1} variant="secondary" align="left" />
+              <MaskText phrases={phrases2} variant="secondary" align="left" />
+              <MaskText phrases={phrases3} variant="secondary" align="left" />
+              <MaskText phrases={phrases4} variant="secondary" align="left" />
+              <MaskText phrases={phrases5} variant="secondary" align="left" />
+              <MaskText phrases={phrases6} variant="secondary" align="left" />
+              <MaskText phrases={phrases7} variant="secondary" align="left" />
+              <MaskText phrases={phrases8} variant="secondary" align="left" />
+            </div>
+          </div>
+        </motion.div>
+        <div className={styles["aboutMe__container__bottomLine"]}></div>
+      </div>
+    </div>
+  );
+};
+
+export default AboutMe;
+
+{
+  /* <p className={styles["aboutMe__info__masksWrapper__paraOne"]}>
             <span
               className={
-                styles["aboutMe__txtContent__paraWrapper__paraOne__hLine"]
+                styles["aboutMe__info__masksWrapper__paraOne__hLine"]
               }
             >
               Hi, I’m a passionate front-end developer <br />
@@ -21,39 +149,23 @@ const AboutMe = () => {
             functionality to deliver solutions that are not only visually
             appealing but also highly effective.
           </p>
-          <p className={styles["aboutMe__txtContent__paraWrapper__paraTwo"]}>
+          <p className={styles["aboutMe__info__masksWrapper__paraTwo"]}>
             Whether it’s building responsive layouts, creating intuitive user
             interfaces, or optimizing websites for performance and SEO, my focus
             is on delivering measurable value to my clients.
             <br />I take pride in solving problems and turning ideas into
             reality with precision and creativity.
           </p>
-          <p className={styles["aboutMe__txtContent__paraWrapper__paraThree"]}>
+          <p className={styles["aboutMe__info__masksWrapper__paraThree"]}>
             I’m committed to staying at the forefront of web development
             practices and helping clients achieve their vision with impactful,
             efficient, and scalable web solutions.
             <span
               className={
-                styles["aboutMe__txtContent__paraWrapper__paraThree__hLine"]
+                styles["aboutMe__info__masksWrapper__paraThree__hLine"]
               }
             >
               Let’s create something amazing together.
             </span>
-          </p>
-        </div>
-      </div>
-      <div className={styles["aboutMe__imageWrapper"]}>
-        <div className={styles["aboutMe__imageWrapper__imgContainer"]}>
-          <img
-            className={styles["aboutMe__imageWrapper__imgContainer__img"]}
-            src={portrait}
-            alt="selfPortrait"
-          />
-        </div>
-        <div>Let’s create something amazing together.</div>
-      </div>
-    </div>
-  );
-};
-
-export default AboutMe;
+          </p> */
+}
---
+++ src/components/MaskText/MaskText.jsx
@@ -0,0 +1,121 @@
+import styles from "./MaskText.module.scss";
+import { motion } from "framer-motion";
+import { useInView } from "react-intersection-observer";
+import PropTypes from "prop-types";
+
+const MaskText = ({ phrases, variant = "primary", align = "left" }) => {
+  const { ref, inView } = useInView({
+    threshold: 1,
+    /* triggerOnce: true, */
+  });
+  //Base class and variants modifiers to keep control of base styles anyways.
+  const alignmentClass = styles[`maskText--${align}`];
+  const maskVariantClass = styles[`maskText--${variant}`];
+  const baseLineMaskClass = styles.maskText__lineMask;
+  const lineMaskVariantClass = styles[`maskText__lineMask--${variant}`];
+  const basePClass = styles.maskText__lineMask__p;
+  const pVariantClass = styles[`maskText__lineMask__p--${variant}`];
+
+  const animation = {
+    initial: { y: "150%" },
+    enter: (i) => ({
+      y: "0%",
+      transition: {
+        duration: 0.75,
+        ease: [0.33, 1, 0.68, 1],
+        delay: 0.05 + i * 0.075,
+      },
+    }),
+  };
+
+  return (
+    <div
+      ref={ref}
+      className={`${styles.maskText} ${alignmentClass} ${maskVariantClass}`}
+    >
+      {phrases.map((phrase, i) => (
+        <div key={i} className={`${baseLineMaskClass} ${lineMaskVariantClass}`}>
+          <motion.p
+            className={`${basePClass} ${pVariantClass}`}
+            custom={i}
+            variants={animation}
+            initial="initial"
+            animate={inView ? "enter" : "initial"}
+          >
+            {phrase}
+          </motion.p>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default MaskText;
+
+MaskText.propTypes = {
+  phrases: PropTypes.arrayOf(PropTypes.string).isRequired,
+  prevPhrases: PropTypes.arrayOf(PropTypes.string),
+  variant: PropTypes.oneOf(["primary", "secondary", "tertiary", "danger"]),
+  align: PropTypes.oneOf(["center", "left", "right"]),
+};
+
+/*
+props
+prevPhrases = [""], 
+
+animation value:
+ delay: 0.05 + (prevPhrases.length - 1) * 0.075  + i * 0.075,
+ 
+ */
+
+/* 
+  console.log(" Check in View " + inView);
+  console.log("Threshold:", 0.75); */
+
+/* import { useEffect, useState, useRef } from "react";
+
+const ManualInView = () => {
+  const ref = useRef(null);
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        console.log("Intersection ratio:", entry.intersectionRatio);
+        setIsVisible(entry.intersectionRatio >= 0.75); // Use threshold of 0.75
+      },
+      { threshold: 0.75 }
+    );
+
+    if (ref.current) observer.observe(ref.current);
+
+    return () => observer.disconnect();
+  }, []);
+
+  return (
+    <div>
+      <div ref={ref} style={{ height: "300px", background: "lightblue" }}>
+        Observe Me
+      </div>
+      <p>{`Is visible: ${isVisible}`}</p>
+    </div>
+  );
+};
+
+export default ManualInView;
+ */
+
+/* 
+  useEffect(() => {
+    if (ref.current) {
+      console.log(ref.current.getBoundingClientRect());
+    }
+  }, [inView, ref,]);
+ */
+
+/*     
+    const ref = useRef(null);
+    const inView = useInView(ref, {
+    threshold: 0.75,
+  }); 
+  */
